35375127b90619211765ce5a99f93792b339c4eb
dylanhernandez1 <hernandez.dylan0622@gmail.com>
2025-01-17 10:32:32 -0800

    Part 3

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -7,7 +7,11 @@ function MyApp() {
 	const [characters, setCharacters] = useState([]);
   
 	function updateList(person) {
-		setCharacters([...characters, person]);
+		postUser(person)
+    .then(() => setCharacters([...characters, person]))
+    .catch((error) => {
+      console.log(error);
+    })
 	}
 
   useEffect(() => {
@@ -26,6 +30,18 @@ function MyApp() {
 
   function fetchUsers() {
     const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  function postUser(person) {
+    const promise = fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
     return promise;
   }
 
